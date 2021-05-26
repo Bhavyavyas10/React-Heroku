79ae552e56c0fc22715db4b85bcd5df2ed159a82
Bhavyavyas10 <bbvyas1082000@gmail.com>
2021-05-26 19:47:48 +0530

    Tweets

--- src/Tweets.js
+++ src/Tweets.js
@@ -1,6 +1,15 @@
 import React from "react";
 import { tweets } from "./data";
 
+const color1 = "#007991";
+const color2 = "#78ffd6";
+
+const color3 = "#4568DC";
+const color4 = "#B06AB3";
+
+const color5 = "#43C6AC";
+const color6 = "#191654";
+
 export const Tweets = () => {
   return (
     <>
@@ -13,19 +22,86 @@ export const Tweets = () => {
                 maintweet={data.title}
                 comment={data.comments.map((item) => (
                   <div>
-                    <div>{item.comment.tweet}</div>
+                    <div
+                      style={{
+                        height: "40px",
+                        background: `linear-gradient(to right,  ${color3},${color4})`,
+                        borderRadius: "10px",
+                        marginBottom: "10px",
+                        display: "flex",
+                        alignItems: "center",
+                        paddingLeft: "20px",
+                        justifyContent: "space-between",
+                      }}
+                    >
+                      <p style={{ color: "#4AC29A", fontSize: "16px" }}>
+                        {item.comment.tweet}
+                      </p>
+
+                      <div style={{ display: "flex", flexDirection: "row" }}>
+                        <input
+                          type="radio"
+                          id="male"
+                          name="gender"
+                          value="male"
+                        />
+                        <label for="male">ABC</label>
+                        <br />
+                        <input
+                          type="radio"
+                          id="female"
+                          name="gender"
+                          value="female"
+                        />
+                        <label for="female">ABC</label>
+                        <br></br>
+                      </div>
+                    </div>
                     {item.hasOwnProperty("replies") ? (
                       <div
-                        style={{ marginLeft: "30px", background: "#C0C0C0" }}
+                        style={{
+                          marginLeft: "30px",
+                          background: `linear-gradient(to right,  ${color5},${color6})`,
+                          marginBottom: "10px",
+                          borderRadius: "10px",
+                        }}
                       >
                         {item.replies.map((e) => (
-                          <div>{e.text}</div>
+                          <div
+                            style={{
+                              display: "flex",
+                              alignItems: "center",
+                              paddingLeft: "20px",
+                              justifyContent: "space-between",
+                            }}
+                          >
+                            {e.text}
+                            <div
+                              style={{ display: "flex", flexDirection: "row" }}
+                            >
+                              <input
+                                type="radio"
+                                id="male"
+                                name="gender"
+                                value="male"
+                              />
+                              <label for="male">ABC</label>
+
+                              <input
+                                type="radio"
+                                id="female"
+                                name="gender"
+                                value="female"
+                              />
+                              <label for="female">ABC</label>
+                              <br></br>
+                            </div>
+                          </div>
                         ))}
                       </div>
                     ) : (
                       <div></div>
                     )}
-                    <div>{console.log(item.hasOwnProperty("replies"))}</div>
                   </div>
                 ))}
               />
@@ -41,22 +117,22 @@ const Tweet = ({ maintweet, comment }) => {
   return (
     <div
       style={{
-        background: "#D3D3D3",
+        background: `linear-gradient(to right,  ${color1},${color2})`,
         marginBottom: "20px",
         borderRadius: "10px",
       }}
     >
       <div style={{ marginLeft: "45px" }}>
-        <h1>Tweet</h1>
-        <h4>{maintweet}</h4>
+        <h1 style={{ color: "#4AC29A" }}>Tweet</h1>
+        <h4 style={{ color: "#021B79", fontSize: "20px" }}>{maintweet}</h4>
       </div>
       <div
         style={{
           marginLeft: "85px",
         }}
       >
-        <h1>Comments</h1>
-        <div style={{ background: "#909090" }}>
+        <h1 style={{ color: "#4AC29A" }}>Comments</h1>
+        <div style={{}}>
           <h6>{comment}</h6>
         </div>
       </div>
